d8b5936a74f7e63930ff7d28c333344dee6bb9da
LitoMore <litomore@gmail.com>
2022-09-21 20:49:05 +0800

    Enhance search function

--- source/components/icons.tsx
+++ source/components/icons.tsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import * as simpleIcons from 'simple-icons/icons'; // eslint-disable-line n/file-extension-in-import
 import getRelativeLuminance from 'get-relative-luminance';
-import {fuzzyMatched} from '../utils.js';
+import {Searcher} from 'fast-fuzzy';
 import Icon from './icon.js';
 
+const icons = Object.values(simpleIcons);
+const searcher = new Searcher(icons, {
+	keySelector: (icon) => [icon.title, icon.slug],
+});
+
 const Icons = ({searchString = ''}: {searchString?: string}) => {
-	const icons = Object.values(simpleIcons);
+	const searchResult = searchString ? searcher.search(searchString) : icons;
 
 	return (
 		<div className="icons">
-			{icons
-				.filter((icon) =>
-					Boolean(
-						fuzzyMatched(icon.title, searchString) ??
-							fuzzyMatched(icon.slug, searchString),
-					),
-				)
-				.map((icon) => {
-					// @ts-expect-error: Expected
-					const luminance = getRelativeLuminance.default(`#${icon.hex}`); // eslint-disable-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-					const light = luminance < 0.4;
+			{searchResult.map((icon) => {
+				// @ts-expect-error: Expected
+				const luminance = getRelativeLuminance.default(`#${icon.hex}`); // eslint-disable-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+				const light = luminance < 0.4;
 
-					return <Icon key={icon.slug} icon={icon} contrastLight={light} />;
-				})}
+				return <Icon key={icon.slug} icon={icon} contrastLight={light} />;
+			})}
 		</div>
 	);
 };
